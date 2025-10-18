b3776ad602feba3b3cee16aab4c2516cc0254be8
KarimAhmed14 <kemoalbaqri@gmail.com>
2025-10-18 21:26:57 +0300

    fixing files

--- data/cart.js
+++ data/cart.js
@@ -1,21 +1,20 @@
-export let cart =JSON.parse(localStorage.getItem('cart'));
-if (!cart) {
-    cart = [{
-      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-      quantity: 2,
-      deliveryOptionsId: '1',
-    }, {
-      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-      quantity: 1,
-      deliveryOptionsId: '1',
-    }];
-  }
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+    deliveryOptionId: '1'
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+    deliveryOptionId: '2'
+  }];
+}
 
 function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
 }
-
 
 export function addToCart(productId) {
   let matchingItem;
@@ -32,10 +31,11 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
+      deliveryOptionId: '1'
     });
-
-    saveToStorage();
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -48,6 +48,20 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
 
   saveToStorage();
 }
