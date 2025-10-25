a4dfc6c88beb48e62bb24f5a14bf7962f437408e
KarimAhmed14 <kemoalbaqri@gmail.com>
2025-10-25 20:58:01 +0300

    load products from the backend

--- data/products.js
+++ data/products.js
@@ -32,7 +32,7 @@ class Product {
   }
 
   getPrice() {
-    return `$${formatCurrency(this.priceCents)}` ;
+    return `$${formatCurrency(this.priceCents)}`;
   }
 
   extraInfoHTML() {
@@ -41,31 +41,107 @@ class Product {
 }
 
 class Clothing extends Product {
-    sizeChartLink;
+  sizeChartLink;
 
-    constructor(productDetails) {
-        super(productDetails);
-        this.sizeChartLink = productDetails.sizeChartLink;
-    }
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
 
-    extraInfoHTML() {
-      super.extraInfoHTML();
-      return `
-        <a href="${this.sizeChartLink}" target="_blank">Size Chart</a>
-      `;
-    }
+  extraInfoHTML() {
+    // super.extraInfoHTML();
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">
+        Size chart
+      </a>
+    `;
+  }
 }
+
 /*
 const date = new Date();
-date.toLocaleDateString();
+console.log(date);
+console.log(date.toLocaleTimeString());
 */
 
+/*
+console.log(this);
+
+const object2 = {
+  a: 2,
+  b: this.a
+};
+*/
+
+/*
 function logThis() {
   console.log(this);
 }
 logThis();
 logThis.call('hello');
 
+this
+const object3 = {
+  method: () => {
+    console.log(this);
+  }
+};
+object3.method();
+*/
+
+export let products = [];
+
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+
+  return promise;
+}
+/*
+loadProductsFetch().then(() => {
+  console.log('next step');
+});
+*/
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+
+    fun();
+  });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -81,8 +157,7 @@ export const products = [
       "sports",
       "apparel"
     ]
-  }  
-  ,
+  },
   {
     id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     image: "images/products/intermediate-composite-basketball.jpg",
@@ -728,7 +803,8 @@ export const products = [
   }
 ].map((productDetails) => {
   if (productDetails.type === 'clothing') {
-      return new Clothing(productDetails);
+    return new Clothing(productDetails);
   }
   return new Product(productDetails);
 });
+*/
